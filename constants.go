4f6c9f1092dddae24de02095391668e4d572e6db
brandon <b.mazey@nyu.edu>
2024-07-05 12:54:20 -0400

    paths

--- constants.go
+++ constants.go
@@ -1,6 +1,6 @@
 package main
 
-// #cgo CFLAGS: -I /interop/mit/src/include
+// #cgo CFLAGS: -I /interop/mit/src/include -ikrb5
 // #cgo LDFLAGS: -L /interop/mit/src/lib -lkrb5
 // #include <krb5.h>
 import "C"
--- principal.go
+++ principal.go
@@ -1,6 +1,6 @@
 package main
 
-// #cgo CFLAGS: -I /interop/mit/src/include
+// #cgo CFLAGS: -I /interop/mit/src/include -ikrb5
 // #cgo LDFLAGS: -L /interop/mit/src/lib -lkrb5
 // #include <stdlib.h>
 // #include <stdio.h>
