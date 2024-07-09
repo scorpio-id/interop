05941da15f852176c7ee41e8ac55c3315cc966fd
brandon <b.mazey@nyu.edu>
2024-07-09 17:13:22 -0400

    LDFLAGS

--- principal.go
+++ principal.go
@@ -1,7 +1,7 @@
 package main
 
 // #cgo CFLAGS: -I /interop/mit/src/include
-// #cgo LDFLAGS: -L /interop/mit/src/lib -Wl,-rpath,$ORIGIN
+// #cgo LDFLAGS: -L /interop/mit/src/lib -Wl,-rpath,$ORIGIN -lkadm5
 // #include <stdlib.h>
 // #include <stdio.h>
 // #include <errno.h>
